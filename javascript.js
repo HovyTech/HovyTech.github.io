bf6a7b303df97018498897a6e08cac6b2edf108e
Alexander Hovy <hovytech@gmail.com>
2015-03-14 14:55:36 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -196,7 +196,8 @@ var getYouTubeCount = function() {
     $('#youtube_count').text(youtube);
   });
 };
-
+//--------------------------------------------------GOOGLE ANALYTICS
+//Provided code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
