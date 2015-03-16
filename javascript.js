aebf5f5ecc4997f1823d142cb2d4361d8fc1734c
Alexander Hovy <hovytech@gmail.com>
2015-03-16 22:10:35 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,12 +9,6 @@ var getDesktop = function() {
   $.getJSON('http://gdata.youtube.com/feeds/api/users/HovyTech/uploads?alt=json', function(data) {
     var html = '';
     var strDescription = '';
-//Get screen dimensions
-    var width = screen.width;
-    var height = screen.height;
-    var rWidth = ((16 / 16 * width) / width) * 100;
-    var rHeight = 9 / 16 * width;
-    var middle = (rHeight / 2) - 15;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
 //Get needed information
@@ -24,11 +18,11 @@ var getDesktop = function() {
       var link = data.feed.entry[i].link[2].href;
       var id = link.substring(31);
       var url = 'https://www.youtube.com/embed/' + id + '?rel=0&showinfo=0&autoplay=1&vq=hd1080&autohide=1';
-      var iframe = '<iframe id=replace' + i + ' height=' + rHeight + 'px width=' + rWidth + '% src=' + url + ' frameborder=0 allowfullscreen></iframe>';
+      var iframe = '<iframe id=replace' + i + ' height=500px width=100% src=' + url + ' frameborder=0 allowfullscreen></iframe>';
       var description = data.feed.entry[i].media$group.media$description.$t;
       var strTitle = '<tr><td><table id="video"><tr><th id="title">' + title + '</th></tr>';
-      var strImage = '<tr><th><img style="height:60px; position:relative; top:' + middle + 'px; width:60px; z-index:25;" onClick="document.getElementById(' + "'" + 'video' + i + "'" + ').innerHTML = ' + "'" + iframe + "'" + '; this.style.visibility=' + "'" + 'hidden' + "'" + ';" src="Images/Play.png"></img>';
-      var strIframe = '<div id="video' + i + '" style="height:' + rHeight + 'px; position:relative; top:-25px; width:' + rWidth + '%; z-index:24;"><img style="height:' + rHeight + 'px; position:relative; top:-25px; width:' + rWidth + '%;" src="' + image + '"></img></div></th></tr>';
+      var strImage = '<tr><th><img style="height:60px; position:relative; top:260px; width:60px; z-index:25;" onClick="document.getElementById(' + "'" + 'video' + i + "'" + ').innerHTML = ' + "'" + iframe + "'" + '; this.style.visibility=' + "'" + 'hidden' + "'" + ';" src="Images/Play.png"></img>';
+      var strIframe = '<div id="video' + i + '" style="height:500px; position:relative; top:-25px; width:100%; z-index:24;"><img style="height:500px; width:100%;" src="' + image + '"></img></div></th></tr>';
       if (description.match('http')) {
         var beginPos = description.search('http');
         var descLink = description.substring(beginPos);
