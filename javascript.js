3c06211cd3d489c3ee0f2ba32e36d93087019706
Alexander Hovy <hovytech@gmail.com>
2015-03-17 22:40:47 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@ var getDesktop = function() {
     var height = screen.height;
     var rWidth = ((16 / 16 * width) / width) * 100;
     var rHeight = ((9 / 16 * width) / height) * 175;
-    var middle = ((9 / 16 * width) / 2) - 140;
+    var middle = ((9 / 16 * width) / 2) - 135;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
 //Get needed information
@@ -56,7 +56,7 @@ var getTablet = function() {
     var height = screen.height;
     var rWidth = ((16 / 16 * width) / width) * 100;
     var rHeight = ((9 / 16 * width) / height) * 315;
-    var middle = ((9 / 16 * width) / 2) + 13;
+    var middle = ((9 / 16 * width) / 2) + 15;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
 //Get needed information
@@ -134,7 +134,7 @@ var getMobile = function() {
     var height = screen.height;
     var rWidth = ((16 / 16 * width) / width) * 100;
     var rHeight = ((9 / 16 * width) / height) * 157;
-    var middle = ((9 / 16 * width) / 2) + 6;
+    var middle = ((9 / 16 * width) / 2) + 10;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
 //Get needed information
