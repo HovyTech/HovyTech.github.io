60f84b2f342b19d5015e6438a1e252689d63af5c
Alexander Hovy <hovytech@gmail.com>
2015-03-17 21:27:15 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ var getDesktop = function() {
     var width = screen.width;
     var height = screen.height;
     var rWidth = ((16 / 16 * width) / width) * 100;
-    var rHeight = ((9 / 16 * width) / height) * 150;
+    var rHeight = ((9 / 16 * height) / height) * 100;
     var middle = (rHeight / 2) - 15;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
@@ -142,7 +142,7 @@ var getMobile = function() {
     var width = screen.width;
     var height = screen.height;
     var rWidth = ((16 / 16 * width) / width) * 100;
-    var rHeight = ((9 / 16 * width) / height) * 150;
+    var rHeight = ((9 / 16 * height) / height) * 100;
     var middle = (rHeight / 2) - 15;
 //Get total amount of videos
     var numVid = data.feed.openSearch$totalResults.$t;
