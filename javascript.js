4f095410b4562a04c9756f77e15caf0cb105a894
Alexander Hovy <hovytech@gmail.com>
2015-03-20 15:59:31 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -217,7 +217,7 @@ var getRRVS = function() {
   });
 //Get Star image
   var digit = description.substring(1, 1);
-  var decimal = description.substring(3, 1);
+  var decimal = description.substring(2, 1);
   alert(digit)
   alert(decimal)
   if (decimal >= 5) {
