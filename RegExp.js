72fd866830eb768f33e6779b948c86603f7d3850
Alexander Hovy <hovytech@gmail.com>
2015-03-25 13:12:08 +0200

    Update RegExp.js

--- RegExp.js
+++ RegExp.js
@@ -1,2 +1,7 @@
+//------------------------------------------------------------HTML
 //var HTRegExp = new RegExp('=".*?"|=' + "'" + '.*?' + "'" + '', 'igm');
+//RegExp
 var = HTRegExp = new RegExp(/\="(.*?)\"|\='(.*?)\'/, 'igm');
+
+//Elements
+var tag = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'city', 'code', 'col', 
