fe2f5848e92c673690423eadcfcae200004bfc93
Alexander Hovy <hovytech@gmail.com>
2015-03-29 10:44:54 +0200

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -11,26 +11,21 @@ var char = ['<', '>', '</', '/>', '="', '"'];
 var charLen = char.length;
 //--------------------HTML
 function preHTML() {
-//var str = document.getElementById('htmla').innerHTML;
-//alert(str)
-var str = $('textarea')[a].val();
-alert(str)
-//var preContents = str.substring(5, str.length - 6);
-//var preContents = str.match(/(<pre>.*?<\/pre>)|(<code>.*?<\/code>)/igm).substring(5, str.length - 6);
-//alert(preContents)
   for (a = 0; a < html; a++) {
     //Get <pre> string 
-    var pre = document.getElementById('html').innerHTML;
-alert(pre)
+    //var pre = document.getElementById('html').innerHTML;
+//alert(pre)
+    var str = $('textarea[name="html-' + a + '"]').val();
+alert(str)
     //Characters
     for (b = 1; b < charLen; b++) {
       //Character count
-      var charCount = pre.split(char[b - 1]).length - 1;
-      var oldPos = pre.indexOf(char[b - 1]);
-      pre.replaceAt(oldPos, '<span id="char">' + char[b - 1] + '</span>');
+      var charCount = str.split(char[b - 1]).length - 1;
+      var oldPos = str.indexOf(char[b - 1]);
+      str.replaceAt(oldPos, '<span id="char">' + char[b - 1] + '</span>');
       for (c = 1; c < charCount; c++) {
-        var newPos = pre.indexOf(char[b - 1], oldPos + 1);
-        pre.replaceAt(newPos, '<span id="char">' + char[b - 1] + '</span>');
+        var newPos = str.indexOf(char[b - 1], oldPos + 1);
+        str.replaceAt(newPos, '<span id="char">' + char[b - 1] + '</span>');
         oldPos = newPos + 1;
       }
     }
