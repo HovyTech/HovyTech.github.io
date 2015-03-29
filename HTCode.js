218be9cc8d0eb3fc98efe16a3f67a947fbc16473
Alexander Hovy <hovytech@gmail.com>
2015-03-29 18:40:23 +0200

    Update HTCode.js

--- HTCode.js
+++ HTCode.js
@@ -1,50 +1,27 @@
 function bodyLoad() {
   //--------------------------------------------------Global Variables
-  //Show numbers = True or False
-  //var showNum = true;
-  //Total number of pre tags in body
-  //var body = document.body.childNodes;
+  //Total number of pre tags
   var total = 1;
-  //for (a = 0; a < body.length; a++) {
-    //if (body[a].nodeName = 'PRE') {
-      //alert(body[a].nodeName)
-      //alert(body[a].nodeType)
-      //total = total + 1;
-    //}
-  //}
   //Store Pre tag text in code array
   var code = [];
   var newCode = [];
-  for (b = 0; b < total; b++) {
-    code.push(document.getElementsByTagName('PRE')[b].childNodes[0].nodeValue);
+  for (a = 0; a < total; a++) {
+    code.push(document.getElementsByTagName('PRE')[a].childNodes[0].nodeValue);
+    newCode.push(code[a]);
   }
+  //Characters
+  var char = ['&lt;', '&lt;/', '&gt;'];
   
   //--------------------------------------------------Clean Up
-  //Sorting code lines
-  //if (showNum = true) {
-    //for (c = 0; c < total; c++) {
-      //newCode.push(code[c]);
-      //newCode[c] = newCode[c].replace(/\n/g, '</li><li>');
-      //newCode[c] = '<ol id="code-num"><li>' + newCode[c] + '</li></ol>';
-      //while (newCode[c].indexOf('<li></li></ol>') != -1) {
-        //newCode[c] = newCode[c].replace('<li></li></ol>', '</ol>');
-      //}
-    //}
-  //} else {
-    //for (c = 0; c < total; c++) {
-      //newCode.push(code[c]);
-      //newCode[c] = newCode[c].replace(/\n/g, '</li><li>');
-      //newCode[c] = '<ul id="code-num"><li>' + newCode[c] + '</li></ul>';
-      //while (newCode[c].indexOf('<li></li></ul>') != -1) {
-        //newCode[c] = newCode[c].replace('<li></li></ul>', '</ul>');
-      //}
-    //}
-  //}
   //Sorting code spaces
-  for (d = 0; d < total; d++) {
-    newCode.push(code[d]);
-    //newCode[d] = newCode[d].replace(/\t/g, '\s\s\s\s');
-    newCode[d] = newCode[d].replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+  for (b = 0; b < total; b++) {
+    newCode[b] = newCode[b].replace(/\t/g, '\s\s\s\s');
+  }
+  //Find characters
+  for (c = 0; c < total; c++) {
+    for (d = 0; d < char.length - 1; d++) {
+      newCode[c] = newCode[c].replace(/char[d]/g, '<span id="char">' + char[d] + '</span>');
+    }
   }
   
   //--------------------------------------------------Insert Clean Code
