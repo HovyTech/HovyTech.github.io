fb63594bdd0522b8feab0345158a6f8b22a0845e
Alexander Hovy <hovytech@gmail.com>
2015-03-29 11:37:18 +0200

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -12,27 +12,20 @@ var charLen = char.length;
 //--------------------HTML
 function preHTML() {
   for (a = 0; a < html; a++) {
-    //Get <pre> string 
-    //var pre = document.getElementById('html').innerHTML;
-//alert(pre)
     var str = document.getElementById('html-' + a).value;
     //Characters
     for (b = 1; b < charLen; b++) {
-alert('')
       //Character count
       var charCount = str.split(char[b - 1]).length - 1;
       var oldPos = str.indexOf(char[b - 1], 0);
       str.replace(oldPos, '<span id="char">' + char[b - 1] + '</span>');
       for (c = 1; c < charCount; c++) {
-alert('')
         var newPos = str.indexOf(char[b - 1], oldPos + 1);
         str.replace(newPos, '<span id="char">' + char[b - 1] + '</span>');
         oldPos = newPos + 1;
-        alert(str)
       }
     }
-    document.getElementById('html-div-' + a).innerHTML = '<pre>' + str + '</pre>';
-    //$('#html-div-' + a).html('<pre>' + str + '</pre>');
+    document.getElementById('html-div-' + a).innerHTML = '<pre id="pre">' + str + '</pre>';
   }
 }
 preHTML();
