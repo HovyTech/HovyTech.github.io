5154936d25790203cbf6ecb04e8d0c949518a07a
Alexander Hovy <hovytech@gmail.com>
2015-03-29 16:03:11 +0200

    Update HTCode.js

--- HTCode.js
+++ HTCode.js
@@ -1,18 +1,41 @@
-//--------------------------------------------------Global Variables
-//Total number of pre tags in body
-var body = document.body.childNodes;
-var total = 0;
-for (a = 0; a < body.length; a++) {
-  if (body[a].nodeName = 'PRE') {
-    total = total + 1;
+function bodyLoad() {
+  //--------------------------------------------------Global Variables
+  //Show numbers = True or False
+  var showNum = true;
+  //Total number of pre tags in body
+  var body = document.body.childNodes;
+  var total = 0;
+  for (a = 0; a < body.length; a++) {
+    if (body[a].nodeName = 'PRE') {
+      total = total + 1;
+    }
+  }
+  //Store Pre tag text in code array
+  var code = [];
+  var newCode = [];
+  for (b = 0; b < total; b++) {
+    code.push(document.getElementsByTagName("PRE")[b].childNodes[0].nodeValue);
+  }
+  
+  //--------------------------------------------------Clean Up
+  //Replacing spaces and new lines
+  if (showNum = true) {
+    for (c = 0; c < total; c++) {
+      newCode.push(code[c]);
+      newCode[c] = newCode[c].replace(/\n/g, '</li><li>');
+      newCode[c] = '<ol id="code-num"><li>' + newCode[c] + '</li></ol>';
+      while (newCode[c].indexOf('<li></li></ol>') != -1) {
+        newCode[c] = newCode[c].replace('<li></li></ol>', '</ol>');
+      }
+    }
+  } else {
+    for (c = 0; c < total; c++) {
+      newCode.push(code[c]);
+      newCode[c] = newCode[c].replace(/\n/g, '</li><li>');
+      newCode[c] = '<ul id="code-num"><li>' + newCode[c] + '</li></ul>';
+      while (newCode[c].indexOf('<li></li></ul>') != -1) {
+        newCode[c] = newCode[c].replace('<li></li></ul>', '</ul>');
+      }
+    }
   }
 }
-//Store Pre tag text in code array
-var code = [];
-for (b = 0; b < total; b++) {
-  code.push(document.getElementsByTagName("PRE")[b].childNodes[0].nodeValue);
-}
-
-//--------------------------------------------------Clean Up
-
-//--------------------------------------------------
