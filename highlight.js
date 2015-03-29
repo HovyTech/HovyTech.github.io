29455ca25445fddab33dff1245b12e1cd530b189
Alexander Hovy <hovytech@gmail.com>
2015-03-29 10:15:23 +0200

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -13,7 +13,7 @@ var charLen = char.length;
 function preHTML() {
 //var str = document.getElementById('htmla').innerHTML;
 //alert(str)
-var str = $('.htmla').html();
+var str = $('.htmla').val();
 alert(str)
 //var preContents = str.substring(5, str.length - 6);
 //var preContents = str.match(/(<pre>.*?<\/pre>)|(<code>.*?<\/code>)/igm).substring(5, str.length - 6);
