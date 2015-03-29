af063538e90ca8cf119c0f86a7deae6b8ac43bf1
Alexander Hovy <hovytech@gmail.com>
2015-03-29 19:08:51 +0200

    Update HTCode.js

--- HTCode.js
+++ HTCode.js
@@ -20,7 +20,7 @@ function bodyLoad() {
   //Find characters
   for (c = 0; c < total; c++) {
     for (d = 0; d < char.length; d++) {
-      newCode[c] = newCode[c].replace(/char[d]/g, '<span id="char">' + char[d] + '</span>');
+      newCode[c] = newCode[c].replace(/char[d]/g, '<span id="char">' + char[d] + ',,</span>');
       alert(char[d])
       alert(newCode[c])
     }
