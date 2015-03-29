8e95ba5dfdf8ab5b9353ce147ac1664172fbb97a
Alexander Hovy <hovytech@gmail.com>
2015-03-29 15:20:58 +0200

    Update CleanLight.js

--- CleanLight.js
+++ CleanLight.js
@@ -1,1 +1,13 @@
-//
+//--------------------------------------------------Global Variables
+//Total number of pre tags in body
+var body = document.body.childNodes;
+var total = 0;
+for (a = 0; a < body.length; a++) {
+  if (body[a].nodeName = 'PRE') {
+    total = total + 1;
+  }
+}
+var code = [];
+for (b = 0; )
+var preHTML = document.getElementsByTagName("PRE")[0].childNodes[0].nodeValue;
+//--------------------------------------------------
