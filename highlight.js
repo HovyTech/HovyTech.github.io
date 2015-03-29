5f48a0b6f3da08d89aba61c8de836fbf71a03704
Alexander Hovy <hovytech@gmail.com>
2015-03-29 13:46:37 +0200

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -7,6 +7,7 @@ var js = 1;
 //Put code in order
 //Characters
 var char = ['<', '>', '</', '/>', '="', '"'];
+var charShort = ['&lt;', '&gt;', '&lt;/', '/&gt;']
 //Length
 var charLen = char.length;
 //--------------------HTML
@@ -18,12 +19,18 @@ function preHTML() {
       //Character count
       var charCount = str.split(char[b - 1]).length - 1;
       var oldPos = str.indexOf(char[b - 1], 0);
-      str.replace(oldPos, '<span id="char">' + char[b - 1] + '</span>');
+      var count = 0;
+      if (count <= 3) {
+        str.replace(oldPos, '<span id="char">' + charShort[count] + '</span>');
+      }
       for (c = 1; c < charCount; c++) {
         var newPos = str.indexOf(char[b - 1], oldPos + 1);
-        str.replace(newPos, '<span id="char">' + char[b - 1] + '</span>');
+        if (count <= 3) {
+          str.replace(newPos, '<span id="char">' + charShort[count] + '</span>');
+        }
         oldPos = newPos + 1;
       }
+      count = count + 1;
     }
     document.getElementById('html-div-' + a).innerHTML = '<pre id="pre">' + str + '</pre>';
   }
