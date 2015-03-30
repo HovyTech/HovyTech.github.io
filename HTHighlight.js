ebeaed56296fc799715b49d734746823657a0745
Alexander Hovy <hovytech@gmail.com>
2015-03-30 15:38:56 +0200

    Update HTHighlight.js

--- HTHighlight.js
+++ HTHighlight.js
@@ -1,3 +1,14 @@
-function loadBody() {
+//Total number of <pre> tags
+var total = 1;
 
+//HTML values
+//var char = /[<>]/g;
+
+function loadBody() {
+  //Load <pre> tag text into array
+  var code = [];
+  for (a = 0; a < total; a++) {
+    code.push(document.getElementsByTagName('PRE')[a].childNodes[0].nodeValue);
+    alert(code[a])
+  }
 }
