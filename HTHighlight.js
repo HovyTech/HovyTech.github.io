f589c8a5ed5874dd4788369b4c4e81a941432214
Alexander Hovy <hovytech@gmail.com>
2015-03-30 15:52:26 +0200

    Update HTHighlight.js

--- HTHighlight.js
+++ HTHighlight.js
@@ -11,6 +11,6 @@ function HTHighlight() {
     code.push(document.getElementsByTagName('PRE')[a].childNodes[0].nodeValue);
     alert(code[a])
   }
-  document.getElementsByTagName('PRE')[0]).innerHTML = 'dfgdf';
+  document.getElementsByTagName('PRE')[0].innerHTML = 'dfgdf';
 }
 HTHighlight();
