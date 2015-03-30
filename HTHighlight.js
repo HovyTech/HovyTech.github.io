a4e836332eaacf9f7ff9a007e4283582503e40f1
Alexander Hovy <hovytech@gmail.com>
2015-03-30 16:30:30 +0200

    Update HTHighlight.js

--- HTHighlight.js
+++ HTHighlight.js
@@ -8,7 +8,9 @@ function HTHighlight() {
   //Load <pre> tag text into array
   alert('')
   var code = [];
+  alert('')
   for (a = 0; a < total; a++) {
+    alert('')
     code.push($('pre').eq(a).html());
     alert(code[a])
   }
