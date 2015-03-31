8852a92caae7bf6fa399743268b90e599526f497
Alexander Hovy <hovytech@gmail.com>
2015-03-31 22:58:26 +0200

    Update HTClean.js

--- HTClean.js
+++ HTClean.js
@@ -1,1 +1,14 @@
- 
+$(function() {
+  var total = 1;
+  var pre = [];
+  
+  for (a = 0; a < total; a++) {
+    pre.push($('pre').eq(a).html());
+  }
+  
+  for (b = 0; b < total; b++) {
+    var broken = [];
+    var start = pre[b].indexOf('&lt;');
+    pre[b].substring();
+  }
+});
