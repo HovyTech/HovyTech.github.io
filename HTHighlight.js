0882a818bef39e4926e2746d7276222950f80189
Alexander Hovy <hovytech@gmail.com>
2015-03-31 22:24:55 +0200

    Update HTHighlight.js

--- HTHighlight.js
+++ HTHighlight.js
@@ -36,6 +36,7 @@ var code = [];
 //var arrText = ['tag', 'att', 'val', 'elm', 'txt', 'com'];
 //Characters
 var char = [/&lt;/ig, /&gt;/ig, /&lt;\//ig, /\/&gt;/ig, /="/ig, /"/ig];
+var charVal = ['&lt;', '&gt;', '&lt;/', '/&gt;', '="', '"'];
 //Tags
 var tag = ['a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'city', 'code', 'col', 'colgroup', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section', 'select', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr'];
 //Attributes
@@ -64,7 +65,7 @@ function HTHighlight() {
     //});
     $(function() {
       for (a = 0; a < char.length; a++) {
-        $('pre').html($('pre').html().replace(char[a], '<span style="color:green">'+char[a]+'</span>'));
+        $('pre').html($('pre').html().replace(char[a],'<span style="color:green">'+charVal[a]+'</span>'));
       }
       //for (b = 0; b < tag.length; b++) {
         //$('pre').html($('pre').html().replace(tag[b], '<span id="tag">'+tag[b]+'</span>'));
