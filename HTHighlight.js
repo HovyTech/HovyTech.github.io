6b2f489230f8f6af883697b04cc79ee3977761ba
Alexander Hovy <hovytech@gmail.com>
2015-03-31 22:06:44 +0200

    Update HTHighlight.js

--- HTHighlight.js
+++ HTHighlight.js
@@ -64,7 +64,7 @@ function HTHighlight() {
     //});
     $(function() {
       for (a = 0; a < char.length; a++) {
-        $('pre').html($('pre').html().replace(tag[a], '<span id="char">' + tag[a] + '</span>'));
+        $('pre').html($('pre').html().replace(char[a], '<span id="char">' + char[a] + '</span>'));
       }
       for (b = 0; b < tag.length; b++) {
         $('pre').html($('pre').html().replace(tag[b], '<span id="tag">' + tag[b] + '</span>'));
