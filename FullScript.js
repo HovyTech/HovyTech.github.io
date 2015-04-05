dc76122d240baadfce3fd26b9578efa607b04a54
Alexander Hovy <hovytech@gmail.com>
2015-04-05 15:16:08 +0200

    Update FullScript.js

--- FullScript.js
+++ FullScript.js
@@ -131,3 +131,13 @@ function getRRVS() {
   });
 };
 getRRVS();
+
+alert('')
+//--------------------------------------------------Highlight
+function highlight() {
+  var tag = /(&lt;|&lt;\/)([\w]+)|&gt;/ig;
+  var c = document.getElementById("html-0").childNodes[0].innerHTML;
+  var dcba = abcd.replace(tag, '<span id="tag">$&</span>';
+  document.getElementById("html-0").innerHTML = dcba;
+}
+highlight();
