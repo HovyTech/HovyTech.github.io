b71bda37d89b0b5323dda87f857128ed42a767f1
Alexander Hovy <hovytech@gmail.com>
2015-04-05 15:37:25 +0200

    Update FullScript.js

--- FullScript.js
+++ FullScript.js
@@ -137,7 +137,7 @@ alert('')
 function highlight() {
   var tag = /(&lt;|&lt;\/)([\w]+)|&gt;/ig;
   var c = document.getElementById("html-0").innerHTML;
-  var dcba = abcd.replace(tag, '<span id="tag">$&</span>';
+  var dcba = abcd.replace(tag, '<span id="tag">$&</span>');
   document.getElementById("html-0").innerHTML = dcba;
 }
 highlight();
